66b8cd6f26c5db28bf98482cc15adf5918e9a195
rowjoy <rowjoy202@gmail.com>
2024-04-25 20:06:52 +0600

    viewOne

---
+++ gameapp/ViewOne.swift
@@ -0,0 +1,18 @@
+//
+//  ViewOne.swift
+//  gameapp
+//
+//  Created by Nec Money on 4/25/24.
+//
+
+import SwiftUI
+
+struct ViewOne: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewOne()
+}
