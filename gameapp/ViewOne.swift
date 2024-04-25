66d0d940e2e93bb90c3e6864b865b8357ee4c501
rowjoy <rowjoy202@gmail.com>
2024-04-25 21:00:04 +0600

    Some new design added

--- gameapp/ViewOne.swift
+++ gameapp/ViewOne.swift
@@ -8,8 +8,75 @@
 import SwiftUI
 
 struct ViewOne: View {
+    
+    func rendomColor ()-> Color {
+        let red = Double.random(in:0...1)
+        let green = Double.random(in: 0...1)
+        let blue = Double.random(in: 0...1)
+        return Color(red : red, green: green, blue : blue)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                ZStack {
+                    Circle()
+                        .frame(width: 50,height: 50)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Image(systemName: "person")
+                        .foregroundColor(.white)
+                }
+                .padding()
+                Spacer()
+                Image(systemName: "bell")
+                    .padding()
+            }
+            
+            ScrollView {
+                VStack {
+                    ForEach(1..<100) { index in
+                        CardView(color: self.rendomColor(), index: index)
+                        
+                    }
+                }
+            }
+            
+            
+        }
+    }
+}
+
+
+struct CardView : View {
+    var color : Color
+    var index : Int
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
+                    .foregroundColor(color)
+                    .frame(height: 100)
+                    .overlay(
+                        HStack {
+                            ZStack (alignment: .center){
+                                Circle()
+                                    .frame(width: 50)
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                    .overlay(
+                                        Text(
+                                            "\(Character(UnicodeScalar(index + 97-1)!))".uppercased()
+                                            
+                                        )
+                                        .foregroundColor(.white)
+                                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                        
+                                        .padding(.trailing, 10)
+                                    )
+                            }
+                            Spacer()
+                            Text("Card \(index)")
+                            
+                                                    }.padding()
+                    )
+                    .padding(10)
     }
 }
 
