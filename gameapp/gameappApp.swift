6551029aa62b532ce6ee68c8639a415700fddf05
rowjoy <rowjoy202@gmail.com>
2024-04-25 20:17:42 +0600

    some change

--- gameapp/gameappApp.swift
+++ gameapp/gameappApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct gameappApp : App {
     var body: some Scene {
         WindowGroup {
-            //ContentView()
-            SettingView()
+           ContentView()
         }
     }
 }
