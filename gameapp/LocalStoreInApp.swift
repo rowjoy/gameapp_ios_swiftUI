b675b762cc1edc7c762f7b2137e1e1b2ed720ab0
rowjoy <rowjoy202@gmail.com>
2024-04-28 15:06:55 +0600

    New add

---
+++ gameapp/LocalStoreInApp.swift
@@ -0,0 +1,18 @@
+//
+//  LocalStoreInApp.swift
+//  gameapp
+//
+//  Created by Nec Money on 4/28/24.
+//
+
+import SwiftUI
+
+struct LocalStoreInApp : View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    LocalStoreInApp()
+}
